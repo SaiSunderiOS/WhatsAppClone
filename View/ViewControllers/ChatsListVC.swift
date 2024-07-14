b68427075a81b759008a608f83d3735ef3d0323b
Sai Sunder <saisunderios@gmail.com>
2024-07-15 01:58:00 +0530

    code refactored

--- View/ViewControllers/ChatsListVC.swift
+++ View/ViewControllers/ChatsListVC.swift
@@ -10,12 +10,16 @@ import UIKit
 
 
 class ChatsListVC: UIViewController {
+    
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var noChatsView: UIView!
     
     var contacts: [NewContacts] = []
+    var messages: [Messages] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        noChatsView.isHidden = false
         tableView.delegate = self
         tableView.dataSource = self
         let nib = UINib(nibName: "ChatsCell", bundle: nil)
@@ -25,6 +29,7 @@ class ChatsListVC: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         fetchContacts()
+        fetchMessages()
     }
     
     private func fetchContacts() {
@@ -34,11 +39,32 @@ class ChatsListVC: UIViewController {
         
         do {
             contacts = try managedContext.fetch(fetchRequest)
+            if contacts.isEmpty {
+                noChatsView.isHidden = false
+            }else{
+                noChatsView.isHidden = true
+            }
             tableView.reloadData()
         } catch {
             print("Failed to fetch contacts: \(error)")
         }
     }
+    
+    func fetchMessages() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Messages> = Messages.fetchRequest()
+        
+        do {
+            messages = try managedContext.fetch(fetchRequest)
+            tableView.reloadData()
+        } catch {
+            print("Failed to fetch messages: \(error)")
+        }
+    }
+    
     
     @IBAction func addContactBtn(_ sender: UIButton) {
         let vc = storyboard?.instantiateViewController(identifier: "NewContactVC") as! NewContactVC
@@ -64,6 +90,19 @@ extension ChatsListVC: UITableViewDelegate, UITableViewDataSource {
             cell.profileImg.image = UIImage(systemName: "person.crop.circle") // Placeholder image if no profile image available
         }
         
+        if !messages.isEmpty {
+            if messages[indexPath.row].contact?.phoneNumber == contact.phoneNumber {
+                if messages.last?.textMessage == nil && messages.last?.image == nil {
+                    cell.messageLbl.text = "No New Messages"
+                }else if messages.last?.textMessage == nil && messages.last?.image != nil {
+                    cell.messageLbl.text = "Sent a photo"
+                }else if messages.last?.textMessage != nil && messages.last?.image == nil{
+                    cell.messageLbl.text = messages.last?.textMessage
+                }
+            }else{
+                cell.messageLbl.text = "No New Messages"
+            }
+        }
         return cell
     }
     
@@ -86,7 +125,11 @@ extension ChatsListVC: UITableViewDelegate, UITableViewDataSource {
                     print("Failed to delete contact: \(error)")
                 }
             }
-            
+            if contacts.isEmpty {
+                noChatsView.isHidden = false
+            }else{
+                noChatsView.isHidden = true
+            }
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
--- View/ViewControllers/NewContactVC.swift
+++ View/ViewControllers/NewContactVC.swift
@@ -58,62 +58,87 @@ class NewContactVC: UIViewController, UIImagePickerControllerDelegate, UINavigat
     }
     
     @IBAction func saveBtn(_ sender: UIButton) {
-        guard let firstName = firstNameTxf.text, !firstName.isEmpty,
-               let lastName = lastNameTxf.text, !lastName.isEmpty,
-               let phoneNumber = phoneNumberTxf.text, !phoneNumber.isEmpty,
-               let status = statusTxf.text, !status.isEmpty,
-               let profileImage = profileImg.image else {
-             // Show an error message or handle the empty fields
-             return
-         }
-
-         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
-         if isFromEdit, let existingContact = contactsInfo {
-             // Editing existing contact
-             existingContact.firstName = firstName
-             existingContact.lastName = lastName
-             existingContact.phoneNumber = phoneNumber
-             existingContact.status = status
-             existingContact.profileImg = profileImage.pngData()
-
-             do {
-                 try context.save()
-                 print("Successfully updated contact")
-                 navigationController?.popToRootViewController(animated: true)
-             } catch {
-                 print("Failed to update contact: \(error)")
-             }
-         } else {
-             // Creating new contact
-             let newContact = NSEntityDescription.insertNewObject(forEntityName: "NewContacts", into: context) as! NewContacts
-
-             newContact.firstName = firstName
-             newContact.lastName = lastName
-             newContact.phoneNumber = phoneNumber
-             newContact.status = status
-             newContact.profileImg = profileImage.pngData()
-
-             do {
-                 try context.save()
-                 print("Successfully saved new contact")
-                 navigationController?.popViewController(animated: false)
-             } catch {
-                 print("Failed to save contact: \(error)")
-             }
-         }
+        guard let firstName = firstNameTxf.text, !firstName.isEmpty else {
+            showToast(message: "Please enter FirstName")
+            return
+        }
+        
+        guard let lastName = lastNameTxf.text, !lastName.isEmpty else {
+            showToast(message: "Please enter LastName")
+            return
+        }
+        
+        guard let phoneNumber = phoneNumberTxf.text, !phoneNumber.isEmpty, phoneNumber.count >= 10, CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: phoneNumber)) else {
+            showToast(message: "Please enter a valid number")
+            return
+        }
+        
+        guard let status = statusTxf.text, !status.isEmpty else {
+            showToast(message: "Please enter status")
+            return
+        }
+        
+        guard let profileImage = profileImg.image else {
+            // Show an error message or handle the empty profile image
+            return
+        }
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        if isFromEdit, let existingContact = contactsInfo {
+            // Editing existing contact
+            existingContact.firstName = firstName
+            existingContact.lastName = lastName
+            existingContact.phoneNumber = phoneNumber
+            existingContact.status = status
+            existingContact.profileImg = profileImage.pngData()
+            
+            do {
+                try context.save()
+                print("Successfully updated contact")
+                navigationController?.popToRootViewController(animated: true)
+            } catch {
+                print("Failed to update contact: \(error)")
+            }
+        } else {
+            // Creating new contact
+            let newContact = NSEntityDescription.insertNewObject(forEntityName: "NewContacts", into: context) as! NewContacts
+            
+            newContact.firstName = firstName
+            newContact.lastName = lastName
+            newContact.phoneNumber = phoneNumber
+            newContact.status = status
+            newContact.profileImg = profileImage.pngData()
+            
+            do {
+                try context.save()
+                print("Successfully saved new contact")
+                navigationController?.popViewController(animated: false)
+            } catch {
+                print("Failed to save contact: \(error)")
+            }
+        }
     }
-    
-    
-//    @IBAction func saveBtn(_ sender: UIButton) {
-//      
-//    }
-
-    
-    
     
     @IBAction func cancelBtn(_ sender: UIButton) {
         navigationController?.popViewController(animated: false)
     }
     
+    func showToast(message: String) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 140, y: 60, width: 280, height: 35))
+        toastLabel.backgroundColor = UIColor.red.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10
+        toastLabel.clipsToBounds = true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 2.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: { (isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
 }
